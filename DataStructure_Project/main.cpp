77458bfceaeef50737448f3bd144f3eb0357178f
meSanjeev <007singhsanjeevkumar@gmail.com>
2021-12-30 01:17:38 +0530

    new commit

--- DataStructure_Project/main.cpp
+++ DataStructure_Project/main.cpp
@@ -1605,6 +1605,84 @@ public:
         
     }
 };
+class Queue_array
+ {
+    int front, rear;
+    int *myqueue;
+    int size;
+public:
+    Queue(){
+        front = -1;
+        rear = -1;
+        size=100;
+        myqueue= new int[size];
+    }
+     
+    bool isFull()
+    {
+        if(front == 0 && rear == size - 1){
+            return true;
+        }
+        return false;
+    }
+     
+    bool isEmpty()
+    {
+        if(front == -1) return true;
+        else return false;
+    }
+     
+    void enQueue(int value)
+    {
+        if(isFull()){
+        cout << endl<< "Queue is full!!";
+        } 
+        else 
+        {
+            if(front == -1) 
+                front = 0;
+            rear++;
+            myqueue[rear] = value;
+            cout << value << " ";
+        }
+    }
+    int deQueue()
+    {
+        int value;
+        if(isEmpty()){
+            cout << "Queue is empty!!" << endl; 
+            return(-1); 
+        } 
+        else { 
+            value = myqueue[front]; 
+            if(front >= rear){ 
+                front = -1;
+                rear = -1;
+            } 
+            else {
+                front++;
+            }
+            cout << endl << "Deleted => " << value << " from myqueue";
+            return(value);
+        }
+    }
+     
+    void displayQueue()
+    { 
+        int i;
+        if(isEmpty()) {
+            cout << endl << "Queue is Empty!!" << endl;
+        }
+        else 
+        {
+            cout << endl << "Front = " << front;
+            cout << endl << "Queue elements : ";
+            for(i=front; i<=rear; i++)
+            cout << myqueue[i] << "\t";
+            cout << endl << "Rear = " << rear << endl;
+        }
+    }
+};
 int main()
 {
     int choice;
